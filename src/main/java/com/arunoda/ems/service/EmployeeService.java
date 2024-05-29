cb3aee9031577f991a7897080ca5f1449f62b4a0
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-05-29 19:21:11 +0530

    Added Some Comments

--- src/main/java/com/arunoda/ems/service/EmployeeService.java
+++ src/main/java/com/arunoda/ems/service/EmployeeService.java
@@ -5,10 +5,13 @@ import com.arunoda.ems.dto.EmployeeDTO;
 import java.util.List;
 
 public interface EmployeeService {
+    //Create Employee
     EmployeeDTO createEmployee(EmployeeDTO employeeDTO);
 
+    //Search Employee By Id
     EmployeeDTO getEmployeeById(Long employeeId);
 
+    //Get All Employees
     List<EmployeeDTO> getAllEmployees();
 
 
